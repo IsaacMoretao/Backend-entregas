88c0906e48e93ce9e9e13202558523c07886f863
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2023-01-05 10:40:52 -0300

    Commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express, { request, response } from "express";
+import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.get("/", (request, response) => {
   return response.json({
